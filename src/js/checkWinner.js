663bcf707bc69e4df5cab47a58ef0ebb337b78c9
Stanislau Vyrski <vyr4376@yandex.ru>
2024-07-27 17:44:39 +0300

    fix: fixed a bug where the game score could not go above one. (Sat, Jul 27, 2024, 5:44:32 PM)

--- src/js/checkWinner.js
+++ src/js/checkWinner.js
@@ -5,7 +5,10 @@ import { setCountValue } from "./setCountValue"
 import { setResultText } from "./setResultText"
 import { winCombo } from "./winCombo"
 
-export const checkWinner = (classList, winElem, winElemCount, winText) => {
+let naughtsScore = 0;
+let crossesScore = 0;
+
+export const checkWinner = (classList, winElem, winText) => {
 	for (let i = 0; i < winCombo.length; i++) {
 		if (fieldItems[winCombo[i][0]].classList.contains(classList) && fieldItems[winCombo[i][1]].classList.contains(classList) && fieldItems[winCombo[i][2]].classList.contains(classList)) {
 			setCountValue(0)
@@ -15,7 +18,7 @@ export const checkWinner = (classList, winElem, winElemCount, winText) => {
 				fieldItems[winCombo[i][0]].classList.add(`${classList}_win`)
 				fieldItems[winCombo[i][1]].classList.add(`${classList}_win`)
 				fieldItems[winCombo[i][2]].classList.add(`${classList}_win`)
-				winElem.textContent = ++winElemCount
+				winElem.textContent = winText === 'xWin' ? ++crossesScore : ++naughtsScore
 				setResultText(winText)
 			}, 250)
 		}
--- src/js/showGameResult.js
+++ src/js/showGameResult.js
@@ -3,11 +3,9 @@ import { checkWinner } from "./checkWinner"
 
 const naughtsScoreElem = document.querySelector('.res__score-naughts')
 const crossesScoreElem = document.querySelector('.res__score-crosses')
-let naughtsScore = 0;
-let crossesScore = 0;
 
 export const showGameResult = () => {
-	checkWinner('field__cross', crossesScoreElem, crossesScore, 'xWin')
-	checkWinner('field__naught', naughtsScoreElem, naughtsScore, 'oWin')
+	checkWinner('field__cross', crossesScoreElem, 'xWin')
+	checkWinner('field__naught', naughtsScoreElem, 'oWin')
 	checkGameDraw()
 }
