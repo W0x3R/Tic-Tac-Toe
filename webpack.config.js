e7238a4e1a439a773d6715ad934f2b22c72fd316
Stanislau Vyrski <114683300+W0x3R@users.noreply.github.com>
2024-11-24 16:01:42 +0300

    feat: added readme and light house images. changed webpack.config (Sun, Nov 24, 2024, 4:01:33 PM)

--- webpack.config.js
+++ webpack.config.js
@@ -1,126 +1,140 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const CopyWebpackPlugin = require("copy-webpack-plugin")
+const TerserPlugin = require("terser-webpack-plugin")
 
-let mode = 'development'
-if (process.env.NODE_ENV === 'production') {
-	mode = 'production'
+let mode = "development"
+if (process.env.NODE_ENV === "production") {
+	mode = "production"
 }
 module.exports = {
-	entry: './src/index.js',
+	entry: "./src/index.js",
 	mode: mode,
 	output: {
-		filename: mode === 'development' ? '[name].js' : '[contenthash].js',
+		filename: mode === "development" ? "[name].js" : "[contenthash].js",
 		assetModuleFilename: "assets/[name][ext][query]",
-		clean: true,
+		clean: true
 	},
 	devServer: {
-		static: './dist',
+		static: "./dist",
 		open: true,
 		hot: true,
-		watchFiles: ['src/**/*'],
+		watchFiles: ["src/**/*"]
 	},
 	performance: {
-		hints: false, // отключить предупреждения о производительности
+		hints: false // отключить предупреждения о производительности
 	},
-	devtool: mode === 'development' ? 'source-map' : false,
-	plugins:
-		[
-			new HtmlWebpackPlugin({
-				template: "./src/index.html",
-			}),
-			new MiniCssExtractPlugin({
-				filename: mode === 'development' ? '[name].css' : '[contenthash].css'
-			}),
-			new CopyWebpackPlugin({
-				patterns: [
-					{ from: './src/images/fav-icons', to: 'assets/images/fav-icons' },
-					{ from: './src/site.webmanifest', to: 'site.webmanifest' },
-					{ from: './src/browserconfig.xml', to: 'browserconfig.xml' }
-				],
-			}),
-		],
+	devtool: mode === "development" ? "source-map" : false,
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "./src/index.html"
+		}),
+		new MiniCssExtractPlugin({
+			filename: mode === "development" ? "[name].css" : "[contenthash].css"
+		}),
+		new CopyWebpackPlugin({
+			patterns: [
+				{ from: "./src/images/fav-icons", to: "assets/images/fav-icons" },
+				{ from: "./src/site.webmanifest", to: "site.webmanifest" },
+				{ from: "./src/browserconfig.xml", to: "browserconfig.xml" },
+				{
+					from: "./src/images/readme/readmePhoto.jpg",
+					to: "assets/images/readme"
+				},
+				{
+					from: "./src/images/readme/lightHouse.jpg",
+					to: "assets/images/readme"
+				}
+			]
+		})
+	],
 	module: {
 		rules: [
 			{
 				test: /\.html$/i,
-				loader: "html-loader",
+				loader: "html-loader"
 			},
 			{
 				test: /\.(sa|sc|c)ss$/,
 				use: [
-					(mode === 'development') ? 'style-loader' :
-						MiniCssExtractPlugin.loader,
-					'css-loader',
+					mode === "development" ? "style-loader" : MiniCssExtractPlugin.loader,
+					"css-loader",
 					{
-						loader: 'postcss-loader',
+						loader: "postcss-loader",
 						options: {
 							postcssOptions: {
-								plugins: [
-									[
-										'postcss-preset-env'
-									]
-								]
+								plugins: [["postcss-preset-env"]]
 							}
 						}
 					},
-					'sass-loader'
+					"sass-loader"
 				]
 			},
 			{
 				test: /\.(png|jpg|jpeg|gif|webp|avif)$/i,
-				type: 'asset/resource',
+				type: "asset/resource",
 				generator: {
-					filename: mode === 'development' ? 'assets/images/pictures/[name][ext]' : 'assets/images/pictures/[contenthash][ext]'
+					filename:
+						mode === "development"
+							? "assets/images/pictures/[name][ext]"
+							: "assets/images/pictures/[contenthash][ext]"
 				}
 			},
 			{
 				test: /\.(svg)$/i,
-				type: 'asset/resource',
+				type: "asset/resource",
 				generator: {
-					filename: mode === 'development' ? 'assets/images/svg/[name][ext]' : 'assets/images/svg/[contenthash][ext]'
+					filename:
+						mode === "development"
+							? "assets/images/svg/[name][ext]"
+							: "assets/images/svg/[contenthash][ext]"
 				}
 			},
 			{
 				test: /\.(woff|woff2|eot|ttf|otf)$/i,
-				type: 'asset/resource',
+				type: "asset/resource",
 				generator: {
-					filename: mode === 'development' ? 'assets/fonts/[name][ext]' : 'assets/fonts/[contenthash][ext]'
+					filename:
+						mode === "development"
+							? "assets/fonts/[name][ext]"
+							: "assets/fonts/[contenthash][ext]"
 				}
 			},
 			{
 				test: /\.m?js$/,
 				exclude: /node_modules/,
 				use: {
-					loader: 'babel-loader',
+					loader: "babel-loader",
 					options: {
-						presets: ['@babel/preset-env']
+						presets: ["@babel/preset-env"]
 					}
 				}
 			},
 			{
 				test: /\.(mp3|wav|ogg)$/i,
-				type: 'asset/resource',
+				type: "asset/resource",
 				generator: {
-					filename: mode === 'development' ? 'assets/sounds/[name][ext]' : 'assets/sounds/[contenthash][ext]'
+					filename:
+						mode === "development"
+							? "assets/sounds/[name][ext]"
+							: "assets/sounds/[contenthash][ext]"
 				}
-			},
+			}
 		]
 	},
 	optimization: {
-		minimize: mode === 'production' ? true : false,
+		minimize: mode === "production" ? true : false,
 		minimizer: [
 			new TerserPlugin({
 				terserOptions: {
 					compress: {
-						drop_console: true,
+						drop_console: true
 					},
 					format: {
-						comments: false,
-					},
-				},
-			}),
-		],
-	},
+						comments: false
+					}
+				}
+			})
+		]
+	}
 }
